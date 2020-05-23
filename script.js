b4703958f8c4d37d2561275078067c0e3dffd8b6
Irina Belousova <irinabelousova11@gmail.com>
2020-05-23 23:29:13 +0400

    More renger sliders were added

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
-const slider1 = document.getElementById("slider1");
-const fill = document.querySelector(".fill");
+// const slider1 = document.getElementById("slider1");
+// const fill = document.querySelector(".fill");
 
-slider1.oninput = function () {
-  fill.style.width = slider1.value + "%";
-  console.log(slider1.value);
+const slider2 = document.getElementById("slider2");
+const slider3 = document.getElementById("slider3");
+
+// slider1.oninput = function () {
+//   fill.style.width = slider1.value + "%";
+//   console.log(slider1.value);
+// };
+
+slider2.oninput = function () {
+  let color = `linear-gradient(
+  90deg,
+  rgb(217, 125, 13) ${slider2.value}%,
+  rgb(68, 68, 68) ${slider2.value}%
+)`;
+
+  slider2.style.background = color;
+};
+
+slider3.oninput = function () {
+  let color = `linear-gradient(#f8dd36, #d88606) no-repeat 0% / ${slider3.value}%`;
+
+  slider3.style.background = color;
 };
