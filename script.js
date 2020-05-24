affdaf13037a08aa60e56dc6248ab80440ebfb64
Irina Belousova <irinabelousova11@gmail.com>
2020-05-25 00:19:01 +0400

    New sliders were added.

--- script.js
+++ script.js
@@ -1,13 +1,7 @@
-// const slider1 = document.getElementById("slider1");
-// const fill = document.querySelector(".fill");
-
 const slider2 = document.getElementById("slider2");
 const slider3 = document.getElementById("slider3");
-
-// slider1.oninput = function () {
-//   fill.style.width = slider1.value + "%";
-//   console.log(slider1.value);
-// };
+const slider5 = document.getElementById("slider5");
+const slider6 = document.getElementById("slider6");
 
 slider2.oninput = function () {
   let color = `linear-gradient(
@@ -23,4 +17,17 @@ slider3.oninput = function () {
   let color = `linear-gradient(#f8dd36, #d88606) no-repeat 0% / ${slider3.value}%`;
 
   slider3.style.background = color;
+};
+
+slider5.oninput = function () {
+  let color = `linear-gradient(rgb(235, 19, 3, 0.8), rgb(179, 15, 3, 0.8)) no-repeat 0% / ${slider5.value}%`;
+
+  slider5.style.background = color;
+};
+
+slider6.oninput = function () {
+  let color = `linear-gradient(rgb(3, 181, 235, 0.8), rgb(3, 137, 179, 0.8))
+  no-repeat 0% /${slider6.value}%`;
+
+  slider6.style.background = color;
 };
